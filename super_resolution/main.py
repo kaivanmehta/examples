31b743ac1ab41dce1154b8066238b1f55ea720ef
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-09-30 13:00:43 -0400

    Update main.py

--- super_resolution/main.py
+++ super_resolution/main.py
@@ -97,7 +97,7 @@ for epoch in range(1, opt.nEpochs + 1):
 
 config_list = [{
     'sparsity': 0.5,
-    'op_types': ['Linear', 'Conv2d']
+    'op_types': ['Conv2d']
 }]
 
 pruner = L1NormPruner(model, config_list)
