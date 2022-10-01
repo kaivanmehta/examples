384163d808ea8b4f4f22e56e5cf72a01789d0157
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-10-01 14:46:17 -0400

    Update main.py

--- mnist_hogwild/main.py
+++ mnist_hogwild/main.py
@@ -49,7 +49,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x), 2)))
+        x = F.relu(F.max_pool2d((self.conv2_drop(self.conv2(x))),2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         #x = F.dropout(x, p = 0.2, training=True)
