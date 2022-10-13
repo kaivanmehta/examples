d7d97b86657b47f05627a2241ed045b5a281ee7c
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-10-13 19:09:25 -0400

    Update main3.py

--- mnist_hogwild/main3.py
+++ mnist_hogwild/main3.py
@@ -114,6 +114,16 @@ if __name__ == '__main__':
     stop = time.time()
     print("before pruning: testing took: ", (stop - start)/60, "minutes!")
     
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    print('model size: {:.3f}MB'.format(size_all_mb))
+    
     print(model)
    
 
