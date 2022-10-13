ffe3886be18fcc22086102fcd77d7a06403b5d47
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-10-13 19:07:36 -0400

    Update main3.py

--- mnist_hogwild/main3.py
+++ mnist_hogwild/main3.py
@@ -138,6 +138,16 @@ if __name__ == '__main__':
     
     print(model)
     
+    param_size = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    buffer_size = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    print('model size: {:.3f}MB'.format(size_all_mb))
+
     processes = []
     start = time.time()
     train(0, args, model, device, dataset1, kwargs)
