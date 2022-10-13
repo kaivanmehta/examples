b3befbf4c62a164fda71b134c33c2961c88dcef1
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-10-13 19:11:45 -0400

    Update main3.py

--- mnist_hogwild/main3.py
+++ mnist_hogwild/main3.py
@@ -117,12 +117,8 @@ if __name__ == '__main__':
     param_size = 0
     for param in model.parameters():
         param_size += param.nelement() * param.element_size()
-    buffer_size = 0
-    for buffer in model.buffers():
-        buffer_size += buffer.nelement() * buffer.element_size()
-
-    size_all_mb = (param_size + buffer_size) / 1024**2
-    print('model size: {:.3f}MB'.format(size_all_mb))
+        
+    print('model size: {:.3f}MB'.format(param_size/1024))
     
     print(model)
    
