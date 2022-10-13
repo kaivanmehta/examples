972704d4d4029805b9fbea70b429216baf09fbef
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-10-13 18:12:21 -0400

    Update main3.py

--- mnist_hogwild/main3.py
+++ mnist_hogwild/main3.py
@@ -9,7 +9,7 @@ from torch.utils.data.sampler import Sampler
 from torchvision import datasets, transforms
 import time
 
-from nni.algorithms.compression.pytorch.quantization import NaiveQuantizer, QAT_Quantizer, DoReFaQuantizer, BNNQuantizer, LsqQuantizer
+from nni.algorithms.compression.pytorch.quantization import NaiveQuantizer, QAT_Quantizer, DoReFaQuantizer, BNNQuantizer, LsqQuantizer, ObserverQuantizer
 
 from train import train, test
 
@@ -131,7 +131,7 @@ if __name__ == '__main__':
     }]
 
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-    DoReFaQuantizer(model, config_list, optimizer = optimizer).compress()
+    ObserverQuantizer(model, config_list).compress()
 #     LsqQuantizer(model, config_list, optimizer = optimizer).compress()
 #     BNNQuantizer(model, config_list, optimizer = optimizer).compress()
     
