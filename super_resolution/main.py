58aac5aab4f026649e1d4b9563628deb1773c0c1
kaivanmehta <52192191+kaivanmehta@users.noreply.github.com>
2022-11-24 23:27:49 -0500

    Update main.py

--- super_resolution/main.py
+++ super_resolution/main.py
@@ -121,7 +121,7 @@ print(model)
 # print(train_set.shape)
 torch.onnx.export(
                 model,
-                torch.tensor(train_set).float(),  
+                torch.tensor(train_set),  
                 "./onnx/super_resolution.onnx", 
                 do_constant_folding=True,
                 input_names=['input'],  # the model's input names (an arbitrary string)
